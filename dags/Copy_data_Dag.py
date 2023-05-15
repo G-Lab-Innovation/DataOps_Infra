acf66e6dbacec6e15aadfc7dd1ab14ae9c92a609
GlabInnovation <129834193+Glabrepo@users.noreply.github.com>
2023-05-15 17:03:05 +0530

    Create Copy_data_Dag.py

---
+++ dags/Copy_data_Dag.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.s3 import S3CopyObjectOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 5, 14),
+    'email': ['atul@glabinnovation.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    's3_copy_objects',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+) as dag:
+
+    start = DummyOperator(task_id='start')
+
+    copy_files = S3CopyObjectOperator(
+        task_id='copy_object',
+        source_bucket_name='airflowtest-bucket-1',
+        source_bucket_key='ds_salaries.csv',
+        dest_bucket_name='airflow-test-bucket-2',
+        dest_bucket_key='ds_copy.csv',
+        aws_conn_id='aws_default'
+    )
+
+    end = DummyOperator(task_id='end')
+
+    start >> copy_files >> end
